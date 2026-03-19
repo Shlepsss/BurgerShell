81d6b119b4dec9e6bc80a0ba2132a5132f505c01
Shlepsss <79642857+Shlepsss@users.noreply.github.com>
2026-03-20 00:52:23 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-wcl -0 -ms -fe=SHELL.EXE main.c libs\graphics.c libs\keyboard.c libs\mouse.c libs\time.c libs\utils.c libs\fs.c libs\sound.c
+wcl -0 -ms -bc -fe=SHELL.COM main.c libs\graphics.c libs\keyboard.c libs\mouse.c libs\time.c libs\utils.c libs\fs.c libs\sound.c
 pause
